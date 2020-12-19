a7f0dc02915b5acf62fdbe4d6e4a0bfb3d0aa7b6
bowouhuy <wibowo55761@gmail.com>
2020-12-19 16:18:37 +0700

    add function cost

--- controller/CoreController.php
+++ controller/CoreController.php
@@ -3,9 +3,13 @@
 
 class api{
     
-    function curl($url,$method){
+    function curl($url,$method,$cost){
 
         $api_url = $url;
+        $xpost="";
+            if($cost!=""){
+                $xpost = "origin=".$cost['origin']."&destination=".$cost['destination']."&weight=".$cost['origin']."&courier=".$cost['courier']."";
+            }
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => $api_url,
@@ -15,7 +19,9 @@ class api{
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_POSTFIELDS => $xpost,
             CURLOPT_HTTPHEADER => array(
+                "content-type: application/x-www-form-urlencoded",
                 "key: acbbd724a63c95cde3c321e1edafee7c"
             )
         ));
@@ -36,13 +42,25 @@ class api{
 
     function daftar_provinsi(){
         $url = "https://api.rajaongkir.com/starter/province";
-        $data = $this->curl($url, "GET");
+        $data = $this->curl($url, "GET","");
         return $data;
     }
 
     function daftar_kota(){
         $url = "https://api.rajaongkir.com/starter/city";
-        $data = $this->curl($url, "GET");
+        $data = $this->curl($url, "GET","");
+        return $data;
+    }
+
+    function cost($origin, $destination, $weight, $courier){
+        $data_cost = [
+            'origin' => $origin,
+            'destination' => $destination,
+            'weight' => $weight,
+            'courier' => $courier
+        ];
+        $url = "https://api.rajaongkir.com/starter/cost";
+        $data = $this->curl($url, "POST",$data_cost);
         return $data;
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 
 <?php
     include "controller/CoreController.php";
+
+    // mengdefiniskan variabel dan diset kosong
+    $origin = $destination = $weight = $courier = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $origin     = $_POST["origin"];
+    $destination    = $_POST["destination"];
+    $weight  = $_POST['weight'];
+    $courier      = $_POST['courier'];
+    }
+
+    $api = new api();
+    $array_kota = $api->daftar_kota();
+    $kota = $array_kota["rajaongkir"]["results"];
 ?>
 
 <!DOCTYPE html>
@@ -12,43 +26,86 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
 <body>
-    <div class="container">
+    <div class="container-md mt-5 col-11">
+        <div class="card mb-3">
+            <div class="card-body" >
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <div class="mb-1 row">
+                        <label for="nama" class="col-sm-2 col-form-label">Kota Asal</label>
+                        <div class="col-sm-10">
+                            <select name="origin" class="form-select" aria-label="Default select example">
+                                <option selected="selected">Pilih</option>
+                                <?php
+                                    foreach ($kota as $row) {
+                                ?>
+                                        <option value="<?php echo $row['city_id']; ?>"><?php echo $row['city_name']; ?></option>
+                                <?php
+                                    }
+                                ?>
+                            </select>
+                            <small id="nama-alert" class="form-text text-danger"></small>
+                        </div>
+                    </div>
+                    <div class="mb-1 row">
+                        <label for="inputNama" class="col-sm-2 col-form-label">Kota Tujuan</label>
+                        <div class="col-sm-10">
+                            <select name="destination" class="form-select" aria-label="Default select example">
+                                <option selected="selected">Pilih</option>
+                                <?php
 
-        <h1>Pilih Kota</h1>
-        <select class="form-select" aria-label="Default select example">
-        <?php
-            $ch = new api();
-            $array_kota = $ch->daftar_kota();
-            $kota = $array_kota["rajaongkir"]["results"];
+                                    foreach ($kota as $row) {
+                                ?>
+                                        <option value="<?php echo $row['city_id']; ?>"><?php echo $row['city_name']; ?></option>
+                                <?php
+                                    }
+                                ?>
+                            </select>
+                            <small id="tanggal-alert" class="form-text text-danger"></small>
+                        </div>
+                    </div>
+                    <div class="mb-1 row">
+                        <label for="nama" class="col-sm-2 col-form-label">Berat (gram)</label>
+                        <div class="col-sm-10">
+                            <input name="weight" type="text" class="form-control" id="inputNama2">
+                            <small id="nama2-alert" class="form-text text-danger"></small>
+                        </div>
+                    </div>
+                    <div class="mb-1 row">
+                        <label for="inputNama" class="col-sm-2 col-form-label">Kurir</label>
+                        <div class="col-sm-10">
+                            <select name="courier" class="form-select" aria-label="Default select example">
+                                <option value="jne">JNE</option>
+                                <option value="tiki">TIKI</option>
+                                <option value="pos">POS Indonesia</option>
+                            </select>
+                            <small id="tanggal2-alert" class="form-text text-danger"></small>
+                        </div>
+                    </div>
+                    <input type="submit" value="Hitung" id="submit" class="btn btn-primary mt-2 col-12 btn-hitung">
+                </form>
+                <?php 
+                    // echo var_dump()
+                    if($origin !=""){
 
-            foreach ($kota as $row) {
-        ?>
-                <option value="<?php echo $row['city_id']; ?>"><?php echo $row['city_name']; ?></option>
+                        $array_result = $api->cost($origin, $destination, $weight, $courier);
+                        $result = $array_result["rajaongkir"]["results"][0]["costs"];
+                        // var_dump($result);          
+                        // var_dump($result);
 
-        <?php
-            }
-            
-            // var_dump($kota);
-            ?>
-            </select>
+                        foreach($result as $a) {
+                            echo $a["service"].' ';
+                            echo $a["description"].'<br>';
 
-            <h1>Pilih Provinsi</h1>
-            <select class="form-select" aria-label="Default select example">
-            <?php
-            $ch = new api();
-            $array_prov = $ch->daftar_provinsi();
-            $provinsi = $array_prov["rajaongkir"]["results"];
+                            foreach($a["cost"] as $b){
+                                echo $b["etd"].' hari <br>';
+                                echo 'Rp. '.$b["value"].'<br>';
 
-            foreach ($provinsi as $row) {
-        ?>
-                <option value="<?php echo $row['province_id']; ?>"><?php echo $row['province']; ?></option>
-
-        <?php
-            }
-            
-            // var_dump($kota);
-            ?>
-            </select>
+                            }
+                        }
+                    }
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
